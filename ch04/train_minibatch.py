c89a0368fb9f83854320eb491b32f9aec018482c
yjoonjang <yjoonjang34@gmail.com>
2024-03-31 21:55:18 +0900

    4.4.1 경사하강법

--- ch04/train_minibatch.py
+++ ch04/train_minibatch.py
@@ -13,3 +13,11 @@ batch_size = 10
 batch_mask = np.random.choice(train_size, batch_size)
 x_batch = x_train[batch_mask]
 t_batch = t_train[batch_mask]
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
